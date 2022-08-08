9bba9b967db62d79701f69ff9d54e172d2a40565
Sachin duhan <38427564+sachin-duhan26@users.noreply.github.com>
2022-08-08 21:40:04 +0530

    updated dynamics component using js

---
+++ components/script.js
@@ -0,0 +1,44 @@
+const SKILLS_CONTAINER = document.getElementById('dynamicSkills');
+
+const USER_SKILLS = [
+    {
+        title: 'skill 1',
+        description: 'Lead React Native developer for Polbol App, a social media for voters, politician & journalists with over 5k',
+        image: 'https://picsum.photos/200'
+    },
+    {
+        title: 'skill 2',
+        description: 'Lead React Native developer for Polbol App, a social media for voters, politician & journalists with over 5k',
+        image: 'https://picsum.photos/200'
+    },
+    {
+        title: 'skill 3',
+        description: 'Lead React Native developer for Polbol App, a social media for voters, politician & journalists with over 5k',
+        image: 'https://picsum.photos/200'
+    },
+    {
+        title: 'skill 4',
+        description: 'Lead React Native developer for Polbol App, a social media for voters, politician & journalists with over 5k',
+        image: 'https://picsum.photos/200'
+    },
+]
+
+const createCards = (skill) => {
+    return `
+    <div class="col-lg-3 col-md-6">
+        <div class="feature_item">
+            <img src="${skill.image}" alt="full stack developer - sachin duhan">
+            <h4>${skill.title}</h4>
+            <p>${skill.description}</p>
+        </div>
+    </div>
+    `;
+}
+
+(function processDynamicSkillsUi(__skills) {
+    if(!__skills || !Array.isArray(__skills)) return;
+    // at this point, we have an array of skills, with length > 0;
+    for(const skill of __skills){
+        SKILLS_CONTAINER.innerHTML += createCards(skill);
+    }
+})(USER_SKILLS);
